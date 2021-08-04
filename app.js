79f3e3b5cb8fbddeea0f6156d2989dcdd10e5e95
Hong Tran <jessietran@fb.com>
2021-08-04 12:57:58 -0700

    updated table

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
+const cors = require('cors')
 
 const taskRoute = require('./api/task/task.route')
 
 app.use(express.json());
+app.use(cors())
 app.use('/task', taskRoute);
 
 module.exports = app;
